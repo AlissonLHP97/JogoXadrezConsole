cf17b8d5794dc238ff95fc1751781bbef67a6337
Alisson Aparecido Pereira <alissonp-alissonp@hotmail.com>
2026-02-18 11:03:00 -0300

    Acrescentando as demais pecas

--- xadrez-console/xadrez/Bispo.cs
+++ xadrez-console/xadrez/Bispo.cs
@@ -2,16 +2,73 @@
 
 namespace xadrez
 {
-    //class Bispo : Peca
-    //{
-    //    public Bispo(Tabuleiro tab, Cor cor) : base(tab, cor)
-    //    {
-            
-    //    }
+    class Bispo : Peca
+    {
+        public Bispo(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
 
-    //    public override string ToString()
-    //    {
-    //        return "";
-    //    }
-    //}   
+        }
+
+
+        public override string ToString()
+        {
+            return "B";
+        }
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+            //Noroeste
+            pos.definirValor(posicao.linha + 1, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValor(pos.linha + 1, pos.coluna + 1);
+            }
+            //Nordeste
+            pos.definirValor(posicao.linha + 1, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValor(pos.linha + 1, pos.coluna + 1);
+            }
+            //Sudeste
+            pos.definirValor(posicao.linha + 1, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValor(pos.linha + 1, pos.coluna + 1);
+            }
+            //Sudoeste
+            pos.definirValor(posicao.linha + 1, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValor(posicao.linha + 1, posicao.coluna - 1);
+            }
+            return mat;
+        }
+    }
 }
